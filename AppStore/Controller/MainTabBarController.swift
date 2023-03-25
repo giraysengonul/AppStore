cb25900e8e37c181c82cad2fe3a160219d17868c
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-03-25 10:44:23 +0300

    Setup

---
+++ AppStore/Controller/MainTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabBarController.swift
+//  AppStore
+//
+//  Created by hakkı can şengönül on 25.03.2023.
+//
+
+import UIKit
+class MainTabBarController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
