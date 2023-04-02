1c3f76960a6a6c4f90c1d3e4530d93dc88eba292
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-04-02 14:24:06 +0300

    AppCellDetailCell Added

--- AppStore/View/AppCellDetailCell.swift
+++ AppStore/View/AppCellDetailCell.swift
@@ -8,6 +8,31 @@
 import UIKit
 class AppCellDetailCell: UICollectionViewCell {
      // MARK: - Properties
+    private let appIcon: UIImageView = {
+       let imageView = UIImageView()
+        imageView.customMode()
+        imageView.backgroundColor = .systemPurple
+        return imageView
+    }()
+    private let nameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "App Name"
+        return label
+    }()
+    private let firmLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Firm Label"
+        return label
+    }()
+    private lazy var getButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Get", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.backgroundColor = UIColor(white: 0.88, alpha: 1)
+        return button
+    }()
+    var labelStackView: UIStackView!
+    var fullStackView: UIStackView!
      // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,9 +47,26 @@ class AppCellDetailCell: UICollectionViewCell {
  // MARK: - Helpers
 extension AppCellDetailCell{
     private func style(){
-        backgroundColor = .cyan
+        labelStackView = UIStackView(arrangedSubviews: [nameLabel,firmLabel])
+        labelStackView.axis = .vertical
+        fullStackView = UIStackView(arrangedSubviews: [appIcon,labelStackView,getButton])
+        fullStackView.axis = .horizontal
+        fullStackView.alignment = .center
+        fullStackView.translatesAutoresizingMaskIntoConstraints = false
+        getButton.layer.cornerRadius = 34 / 2
+        appIcon.layer.cornerRadius = 10
     }
     private func layout(){
-        
+        addSubview(fullStackView)
+        NSLayoutConstraint.activate([
+            appIcon.widthAnchor.constraint(equalToConstant: 80),
+            appIcon.heightAnchor.constraint(equalToConstant: 80),
+            getButton.widthAnchor.constraint(equalToConstant: 80),
+            getButton.heightAnchor.constraint(equalToConstant: 34),
+            fullStackView.topAnchor.constraint(equalTo: topAnchor),
+            fullStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            fullStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            fullStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
     }
 }
