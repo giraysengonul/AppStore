275a87a9f75b3117a0ea370aebcc5b691bd631cd
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-04-06 00:47:46 +0300

    TodayDetailViewController Added

---
+++ AppStore/Controller/Today/TodayDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TodayDetailViewController.swift
+//  AppStore
+//
+//  Created by hakkı can şengönül on 6.04.2023.
+//
+
+import UIKit
+class TodayDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        view.layer.cornerRadius = 14
+    }
+}
--- AppStore/Controller/Today/TodayViewController.swift
+++ AppStore/Controller/Today/TodayViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 private let reuseIdentifier = "TodayCell"
 class TodayViewController: UICollectionViewController {
      // MARK: - Properties
+    private var startLocation: CGRect?
+    private let todayDetailViewController = TodayDetailViewController()
      // MARK: - Lifecycle
      init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
@@ -37,6 +39,38 @@ extension TodayViewController{
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
         return cell
+    }
+}
+ // MARK: - Actions
+extension TodayViewController{
+    @objc private func handleTodayDetailViewController(_ sender: UITapGestureRecognizer){
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8) {
+            self.todayDetailViewController.view.frame = self.startLocation ?? .zero
+            self.tabBarController?.tabBar.alpha = 1
+        } completion: { _ in
+            self.todayDetailViewController.removeFromParent()
+            self.todayDetailViewController.view.removeFromSuperview()
+        }
+
+    }
+}
+ // MARK: - UICollectionViewDelegate
+extension TodayViewController{
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = collectionView.cellForItem(at: indexPath)
+        self.startLocation = item?.superview?.convert(item?.frame ?? .zero, to: nil)
+        todayDetailViewController.view.frame = startLocation ?? .zero
+        self.todayDetailViewController.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTodayDetailViewController)))
+        addChild(todayDetailViewController)
+        view.addSubview(todayDetailViewController.view)
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8) {
+            self.todayDetailViewController.view.frame = self.view.frame
+            self.tabBarController?.tabBar.alpha = 0
+        } completion: { _ in
+            
+        }
+
+        
     }
 }
  // MARK: - UICollectionViewDelegateFlowLayout
